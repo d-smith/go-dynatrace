42c4709779c95cf325d2f51ed20543d829ce1bcd
d-smith <doug.smith.mail@gmail.com>
2015-04-23 20:51:33 +0000

    First version injecting method names in pure path

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "math/rand"
   "net/http"
   "time"
@@ -9,6 +10,7 @@ import (
 /*
 #cgo CFLAGS: -I/home/vagrant/dynatrace-6.1.0/adk/include
 #cgo LDFLAGS: -L/home/vagrant/dynatrace-6.1.0/adk/lib64 -l dtadk
+#include <stdlib.h>
 #include <dynatrace_adk.h>
 
 struct DTContext {
@@ -27,23 +29,19 @@ DYNATRACE_INITIALIZE(&argCount,&argv);
 
 }
 
-struct DTContext enter_dt()
+struct DTContext enter_dt(char *methodName)
 {
-  //TODO - figure out how to specify method name, not pick it up via __FUNCTION__
   //DYNATRACE_API("Gopher");
-  static const char *__dynatrace_api__ = "Gopher";
+  static const char *__dynatrace_api__ = "Xavi the Wonder Pony";
 
   //DYNATRACE_START_PUREPATH();
   static int __dynatrace_method_id__ = 0;
   int __dynatrace_serial_no__;
-  if (!__dynatrace_method_id__) {
-		__dynatrace_method_id__ = dynatrace_get_method_id("colonol malfunction", "bad finger", 666, __dynatrace_api__, 0);
-	}
+  //if (!__dynatrace_method_id__) {
+		__dynatrace_method_id__ = dynatrace_get_method_id(methodName, "XAVI", 666, __dynatrace_api__, 0);
+	//}
 	__dynatrace_serial_no__ = dynatrace_get_serial_no(__dynatrace_method_id__, 1);
   __dynatrace_serial_no__ = dynatrace_enter(__dynatrace_method_id__, __dynatrace_serial_no__);
-
-
-
 
   struct DTContext ctx = {__dynatrace_method_id__, __dynatrace_serial_no__};
   return ctx;
@@ -53,7 +51,6 @@ void exit_dt(struct DTContext ctx)
 {
   int __dynatrace_method_id__ = ctx.methodId;
   int __dynatrace_serial_no__ = ctx.serialNo;
-
 
   //DYNATRACE_EXIT();
 
@@ -67,6 +64,7 @@ void exit_dt(struct DTContext ctx)
 
 */
 import "C"
+import "unsafe"
 
 
 func init() {
@@ -75,7 +73,11 @@ func init() {
 }
 
 func clientCall(w http.ResponseWriter) {
-  ctx := C.enter_dt()
+  name := C.CString("clientCall")
+  defer C.free(unsafe.Pointer(name))
+  ctx := C.enter_dt(name)
+  fmt.Printf("method id: %v sequence: %v\n", ctx.methodId, ctx.serialNo)
+
 
   delayBase := 1 + rand.Intn(5)
   time.Sleep(time.Duration(delayBase*300) * time.Millisecond)
@@ -86,7 +88,10 @@ func clientCall(w http.ResponseWriter) {
 
 
 func handleCall(w http.ResponseWriter, r *http.Request) {
-  ctx := C.enter_dt()
+  name := C.CString("handleCall")
+  defer C.free(unsafe.Pointer(name))
+  ctx := C.enter_dt(name)
+  fmt.Printf("method id: %v sequence: %v\n", ctx.methodId, ctx.serialNo)
 
   delayBase := 1 + rand.Intn(5)
   time.Sleep(time.Duration(delayBase*100) * time.Millisecond)
