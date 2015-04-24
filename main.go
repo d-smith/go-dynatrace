868cd713440f3cf72c82a352965dfcbbd8021b09
d-smith <doug.smith.mail@gmail.com>
2015-04-24 14:00:02 +0000

    Version with name allocation per call, no method id reuse

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
   "fmt"
   "math/rand"
   "net/http"
-  "sync"
   "time"
 )
 
@@ -37,21 +36,15 @@ static void init_dt()
 
 //TODO - install a shutdown hook to cleanly disconnect from dynatrace
 
-// Call this in go init method to get the method id to use for calls.
-// This keeps us  from calling the dynatrace_get_method redundantly,
-// which the macro from in the ADK avoids and reduces the amount
-// of string allocation significantly.
-int getDynatraceMethodId(char *methodName) {
-  return dynatrace_get_method_id(methodName, "XAVI II", 666, "Gopher3", 0);
-}
 
 // Start a pure path contribution
-struct DTContext enter_dt(int methodId)
+struct DTContext enter_dt(char *methodName)
 {
   //DYNATRACE_API("Gopher");
 
   //DYNATRACE_START_PUREPATH();
   int __dynatrace_serial_no__;
+  int methodId = dynatrace_get_method_id(methodName, "XAVI II", 666, "Gopher3", 0);
 	__dynatrace_serial_no__ = dynatrace_get_serial_no(methodId, 1);
   __dynatrace_serial_no__ = dynatrace_enter(methodId, __dynatrace_serial_no__);
 
@@ -75,35 +68,19 @@ void exit_dt(struct DTContext ctx)
 import "C"
 import "unsafe"
 
-// Variables used to hold dynatrace method ids for instrumented methods
-var (
-  handleCallId C.int
-  childCallId C.int
-)
 
-// Mutex for synchronizing Dynatrace ADK calls
-var mutex sync.Mutex
 
 //Initialize the dynatrace method ids
 func init() {
   println("init here")
   C.init_dt()
-
-  handleCall := C.CString("handleCall")
-  defer C.free(unsafe.Pointer(handleCall))
-  handleCallId = C.getDynatraceMethodId(handleCall)
-
-  childCall := C.CString("childCall")
-  defer C.free(unsafe.Pointer(childCall))
-  childCallId = C.getDynatraceMethodId(childCall)
-
 }
 
 //Sample child method called from handleCall - shown as descendant in pure path
 func childCall(w http.ResponseWriter) {
-  mutex.Lock()
-  ctx := C.enter_dt(childCallId)
-  mutex.Unlock()
+  fnName := C.CString("childCall")
+  defer C.free(unsafe.Pointer(fnName))
+  ctx := C.enter_dt(fnName)
   fmt.Printf("method id: %v sequence: %v\n", ctx.methodId, ctx.serialNo)
 
 
@@ -111,17 +88,15 @@ func childCall(w http.ResponseWriter) {
   time.Sleep(time.Duration(delayBase*300) * time.Millisecond)
 	w.Write([]byte("Here's some content, dog\n"))
 
-  mutex.Lock()
   C.exit_dt(ctx)
-  mutex.Unlock()
 }
 
 //Top level pure path call
 func handleCall(w http.ResponseWriter, r *http.Request) {
 
-  mutex.Lock()
-  ctx := C.enter_dt(handleCallId)
-  mutex.Unlock()
+  fnName := C.CString("handleCall")
+  defer C.free(unsafe.Pointer(fnName))
+  ctx := C.enter_dt(fnName)
   fmt.Printf("method id: %v sequence: %v\n", ctx.methodId, ctx.serialNo)
 
   delayBase := 1 + rand.Intn(5)
@@ -130,9 +105,7 @@ func handleCall(w http.ResponseWriter, r *http.Request) {
 
   childCall(w);
 
-  mutex.Lock()
   C.exit_dt(ctx)
-  mutex.Unlock()
 
 }
 
