505923e93ad63d7e6434b0f96b31d3eaf96b0a82
d-smith <doug.smith.mail@gmail.com>
2015-05-02 21:35:50 +0000

    Did some cleanup and some more docs

--- sidecar/main.go
+++ sidecar/main.go
@@ -1,19 +1,17 @@
 package main
 
-
-
 import (
-	"github.com/d-smith/go-dynatrace/sidecar/server"
 	"net/http"
+
+	"github.com/d-smith/go-dynatrace/sidecar/server"
 )
-	
+
 //Serve up some http goodness
 func serveHttp() {
-  handleCallHandler := http.HandlerFunc(server.HandleCall)
-  http.ListenAndServe(":8080", handleCallHandler)
+	handleCallHandler := http.HandlerFunc(server.HandleCall)
+	http.ListenAndServe(":8080", handleCallHandler)
 }
 
-
 func main() {
-  serveHttp()
+	serveHttp()
 }
--- sidecar/timing/timing.go
+++ sidecar/timing/timing.go
@@ -8,13 +8,11 @@ package timing
 #include <stdio.h>
 #include <time.h>
 
+//C code for inserting golang information into the Dynatrace pure path.
+
 //Initialize dynatrace
 static void init_dt()
 {
-  //TODO - look at the initialize macro and determine the
-  //correct set up. Might be able to handle everything via environment
-  //variables.
-
   int argCount = 2;
   char *strs[2] = {"dummy","--dt_debugadk=true"};
   char **argv = &strs;
@@ -30,34 +28,33 @@ typedef struct {
 } TimingData;
 
 void subcall(int subcallTime) {
-	DYNATRACE_ENTER();
 	char buf[256];
 	sprintf(buf, "proxied service call time: %d ms", subcallTime);
+
+	DYNATRACE_ENTER();
 	DYNATRACE_LOG(0, buf, "xavi");
 	DYNATRACE_EXIT();
 }
 
 void api(int apiTime, int subcallTime) {
-	DYNATRACE_ENTER();
 	char buf[256];
 	sprintf(buf, "api call time: %dms", apiTime);
+
+	DYNATRACE_ENTER();
 	DYNATRACE_LOG(0, buf, "xavi");
 	subcall(subcallTime);
 	DYNATRACE_EXIT();
 }
 
 static void handleTiming(TimingData td) {
-	
 	DYNATRACE_API("Son of Sidecar");
 	DYNATRACE_START_PUREPATH();
-	//printf("Name: %s, timing: %d\n", td.name, td.timing);
-	//printf("Subname: %s, subtiming: %d\n", td.subname, td.subtiming);
 	api(td.timing, td.subtiming);
 	DYNATRACE_EXIT();
 }
 
 
-
+//TODO: Add dt shutdown hook.
 
 
 */
@@ -69,7 +66,7 @@ func init() {
 }
 
 type CallTime struct {
-	Name string
+	Name   string
 	Millis int
 }
 
@@ -82,15 +79,25 @@ type ServiceTimer struct {
 	ServiceTime
 }
 
+//WriteTimingData composes a C structure to pass to the C code above that uses the
+//Dynatrace ADK C macros. Since the C macros rely on thread local storage, the work they do
+//needs to happen on the same thread: if general calls are made using the Dynatrace ADK
+//in a golang program, the pure path will become corrupted anytime the go scheduler suspends
+//a thread, as the thread context gets overwritten. This is because the golang scheduler multiplexs
+//logical threads onto physical threads.
 func WriteTimingData(svcTiming ServiceTimer) {
+
 	var td C.TimingData
+
+	//Timing context for outer measurement point (Xavi entry/exit timing)
 	td.name = C.CString(svcTiming.Name)
 	defer C.free(unsafe.Pointer(td.name))
 	td.timing = C.int(svcTiming.Millis)
+
+	//Timing context for inner context (proxied service call). TODO: handle multiple inner timing points.
 	td.subname = C.CString(svcTiming.ChildCalls[0].Name)
 	defer C.free(unsafe.Pointer(td.subname))
 	td.subtiming = C.int(svcTiming.ChildCalls[0].Millis)
 
 	C.handleTiming(td)
 }
-
