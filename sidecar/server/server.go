315f08c12ad1e096d4d029d62c4dae9447c08309
d-smith <doug.smith.mail@gmail.com>
2015-05-07 13:58:50 +0000

    Cleanup server code

--- sidecar/server/server.go
+++ sidecar/server/server.go
@@ -2,40 +2,49 @@ package server
 
 import (
 	"fmt"
-	"net/http"
-	"math/rand"
-	"time"
 	"github.com/d-smith/go-dynatrace/sidecar/timing"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
 func childCall(w http.ResponseWriter) {
 
-  fmt.Printf("childCall\n")
+	fmt.Printf("childCall\n")
 
-  delayBase := 1 + rand.Intn(5)
-  time.Sleep(time.Duration(delayBase*300) * time.Millisecond)
-  w.Write([]byte("Here's some child content, dog\n"))
+	delayBase := 1 + rand.Intn(5)
+	time.Sleep(time.Duration(delayBase*300) * time.Millisecond)
+	w.Write([]byte("Here's some child content, dog\n"))
 }
 
 //HandleCall handles http service requests
 func HandleCall(w http.ResponseWriter, r *http.Request) {
+
+	//Start API timing
 	var svcTiming timing.ServiceTimer
 	svcTiming.Name = "HandleCall"
 	now := time.Now()
- 
-  fmt.Printf("HandleCall")
 
-  delayBase := 1 + rand.Intn(5)
-  time.Sleep(time.Duration(delayBase*100) * time.Millisecond)
+	//Simulate processing in the API layer
+	fmt.Println("HandleCall")
+	delayBase := 1 + rand.Intn(5)
+	time.Sleep(time.Duration(delayBase*100) * time.Millisecond)
 	w.Write([]byte("Here's some content, dog\n"))
+
+	//Start proxied service timing
 	childStart := time.Now()
-    childCall(w);
-    childElapsed := time.Since(childStart)
-    childCallTime := timing.CallTime{"childCall", int(childElapsed/1000000)}
 
-    svcTiming.Millis = int(time.Since(now)/1000000)
-    svcTiming.ChildCalls = append(svcTiming.ChildCalls, childCallTime)
+	//Simulate proxied service call
+	childCall(w)
 
-    
-    go timing.WriteTimingData(svcTiming)
+	//Capture service call timing
+	childElapsed := time.Since(childStart)
+	childCallTime := timing.CallTime{"childCall", int(childElapsed / 1000000)}
+
+	//Capture api timing
+	svcTiming.Millis = int(time.Since(now) / 1000000)
+	svcTiming.ChildCalls = append(svcTiming.ChildCalls, childCallTime)
+
+	//Send timing data to go routine for processing
+	go timing.WriteTimingData(svcTiming)
 }
